d0b2fb1bebe4ad5cf374152e154d39d7be481696
Steinimfluss <tom.p.kuehner@outlook.de>
2026-01-16 21:41:27 +0100

    Add redirecting to limbo

--- velocity/src/main/java/me/tom/velocity/App.java
+++ velocity/src/main/java/me/tom/velocity/App.java
@@ -1,13 +1,43 @@
 package me.tom.velocity;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.ServerPreConnectEvent;
+import com.velocitypowered.api.event.player.ServerPreConnectEvent.ServerResult;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+
+@Plugin(
+        id = "cascade",
+        name = "Cascade",
+        version = "1.0"
+)
+public class App {
+	private final ArrayList<UUID> authUsers = new ArrayList<>();
+
+    private final RegisteredServer cascade;
+    private final Logger logger;
+
+    @Inject
+    public App(ProxyServer proxy, Logger logger) {
+        this.logger = logger;
+        
+        cascade = proxy.getServer("cascade")
+                .orElseThrow(() -> new IllegalStateException("Cascade server not found!"));
+    }
+
+    @Subscribe
+    public void onServerPreConnect(ServerPreConnectEvent event) {
+    	if(!authUsers.contains(event.getPlayer().getUniqueId())) {
+    		event.setResult(ServerResult.allowed(cascade));
+            logger.info("Redirected {} to cascade", event.getPlayer().getUsername());
+    	}
     }
 }
