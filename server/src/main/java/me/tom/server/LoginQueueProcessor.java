539325454dd92b7cbc640fb3eddf5cebd02b11e0
Steinimfluss <tom.p.kuehner@outlook.de>
2026-01-20 16:16:10 +0100

    Improve login queueing

--- server/src/main/java/me/tom/server/LoginQueueProcessor.java
+++ server/src/main/java/me/tom/server/LoginQueueProcessor.java
@@ -13,20 +13,37 @@ import me.tom.common.network.protocol.packet.types.Utf8String;
 public class LoginQueueProcessor {
     public static void start(EventLoop eventLoop) {
         eventLoop.scheduleAtFixedRate(() -> {
-            ChannelHandlerContext ctx = LoginQueue.poll();
-            if (ctx == null) return;
 
-            ByteBuf buf = ctx.alloc().buffer();
-    	    Utf8String.write(buf, ctx.channel().attr(ProtocolAttributes.USERNAME).get(), 16);
+            while (true) {
+                ChannelHandlerContext ctx = LoginQueue.poll();
+                if (ctx == null) return;
 
-    	    byte[] payload = new byte[16];
-    	    int len = buf.readableBytes();
+                if (!ctx.channel().isActive() || !ctx.channel().isOpen()) {
+                    continue;
+                }
 
-    	    buf.readBytes(payload, 0, len);
-    	    buf.release();
+                ByteBuf buf = ctx.alloc().buffer();
+                Utf8String.write(buf, ctx.channel().attr(ProtocolAttributes.USERNAME).get(), 16);
 
-    	    ctx.writeAndFlush(new PluginMessage("cascade:login", payload));
-    	    ctx.writeAndFlush(new TransferPacket(ctx.channel().attr(ProtocolAttributes.HOSTNAME).get(), ctx.channel().attr(ProtocolAttributes.PORT).get()));
-        }, 0, 10, TimeUnit.MILLISECONDS);
+                int len = buf.readableBytes();
+                byte[] payload = new byte[len];
+                buf.readBytes(payload);
+                buf.release();
+                
+                ctx.writeAndFlush(new PluginMessage("cascade:login", payload))
+                .addListener(f -> {
+                    if (!f.isSuccess()) {
+                        f.cause().printStackTrace();
+                    }
+                });
+                ctx.writeAndFlush(new TransferPacket(
+                        ctx.channel().attr(ProtocolAttributes.HOSTNAME).get(),
+                        ctx.channel().attr(ProtocolAttributes.PORT).get()
+                ));
+
+                ctx.close();
+                return;
+            }
+        }, 0, 50, TimeUnit.MILLISECONDS);
     }
 }
